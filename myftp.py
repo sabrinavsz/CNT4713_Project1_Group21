177540db2adccae6db7c2fa27ee947fad48b6723
Sabrina Vasquez <sabrinamichellevasquez@gmail.com>
2026-02-12 01:50:42 -0500

    Implemented login and quit features in skeleton

--- myftp.py
+++ myftp.py
@@ -10,7 +10,7 @@ from socket import *
 import sys # In order to terminate the program
 
 def quitFTP(clientSocket):
-    # COMPLETE
+    command = "QUIT\r\n"
     dataOut = command.encode("utf-8")
     clientSocket.sendall(dataOut)
     dataIn = clientSocket.recv(1024)
@@ -18,8 +18,10 @@ def quitFTP(clientSocket):
     print(data)
 
 def sendCommand(socket, command):
+    command = command + "\r\n"
     dataOut = command.encode("utf-8")
-    # Complete
+    socket.sendall(dataOut)
+    data = " "
     return data
 
 
@@ -31,14 +33,14 @@ def receiveData(clientSocket):
 
 # If you use passive mode you may want to use this method but you have to complete it
 # You will not be penalized if you don't
-def modePASV(clientSocket):
-    command = "PASV" + "\r\n"
+#def modePASV(clientSocket):
+    #command = "PASV" + "\r\n"
     # Complete
-    status = 0
-    if data.startswith(""):
-        status = 227
+    #status = 0
+    #if data.startswith(""):
+        #status = 227
         # Complete
-        dataSocket.connect((ip, port))
+        #dataSocket.connect((ip, port))
         
     return status, dataSocket
 
@@ -51,45 +53,52 @@ def main():
     password = input("Enter the password: ")
 
     clientSocket = socket(AF_INET, SOCK_STREAM) # TCP socket
-    # COMPLETE
-
-    HOST = # COMPLETE
-    # COMPLETE
+    
+    HOST = input("Enter the server address: ")
+    PORT = 21
+    
+    clientSocket.connect((HOST,PORT))
 
     dataIn = receiveData(clientSocket)
     print(dataIn)
 
     status = 0
     
-    if dataIn.startswith(""):
+    if dataIn.startswith("220"):
         status = 220
         print("Sending username")
-        # COMPLETE
+        sendCommand(clientSocket, "USER " + username)
         
+        dataIn = receiveData(clientSocket)
         print(dataIn)
 
         print("Sending password")
-        if dataIn.startswith(""):
+        if dataIn.startswith("331"):
             status = 331
-            # COMPLETE
+            sendCommand(clientSocket, "PASS " + password)
             
+            dataIn = receiveData(clientSocket)
             print(dataIn)
-            if dataIn.startswith(""):
+
+            if dataIn.startswith("230"):
+                print("Succesfully logged in.")
                 status = 230
+            else:
+                print("Login unsuccessful.")
 
        
-    if status == 230:
+    #if status == 230:
         # It is your choice whether to use ACTIVE or PASV mode. In any event:
         # COMPLETE
-        pasvStatus, dataSocket = modePASV(clientSocket)
-        if pasvStatus == 227:
+        #pasvStatus, dataSocket = modePASV(clientSocket)
+        #if pasvStatus == 227:
             # COMPLETE
     
     print("Disconnecting...")
     
 
     clientSocket.close()
-    dataSocket.close()
+    #dataSocket.close()
     
     sys.exit()#Terminate the program after sending the corresponding data
 
